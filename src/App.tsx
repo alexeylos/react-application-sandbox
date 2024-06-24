4d83806d54e8847bac31f68c73a387e0b20a61c4
Mainul2 Fahim <m2.fahim@itransition.com>
2024-06-24 15:07:32 +0600

    prettier lint staged husky added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import ".//styles.less";
-import { Button } from "antd";
+import './/styles.less';
+import { Button, Flex } from 'antd';
 // import "antd/dist/antd.less";
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 const App = () => {
-  // const hello = 0;
   return (
-    <div>
-      <h1>Hello world</h1>
-      <Button type="primary">Primary Button</Button>
-    </div>
+    <Flex gap="middle" align="center" vertical>
+      <Flex justify="center" align="center">
+        <Title>Hello World</Title>
+      </Flex>
+      <Flex gap="middle" justify="center" align="center">
+        <Button type="primary">Primary Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+      </Flex>
+    </Flex>
   );
 };
 
