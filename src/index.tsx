44c8e53daab364f9b42e901e6b383b20a2fb6064
Mainul2 Fahim <m2.fahim@itransition.com>
2024-06-25 17:09:38 +0600

    Pushed missing changes husky=>pre-commit,tsconfig.json,eslintrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root');
 
@@ -8,4 +11,9 @@ if (!rootElement) {
 }
 
 const root = createRoot(rootElement);
-root.render(<App />);
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
